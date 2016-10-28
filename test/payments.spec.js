871c5efe677e27af3072611bc989a25e5d5c2d9b
Salama AB <aksalj@aksalj.com>
2016-10-28 09:59:12 +0300

    Minor clean up

--- test/payments.spec.js
+++ test/payments.spec.js
@@ -9,6 +9,15 @@ let expect = chai.expect;
 const PesaJs = require('../index');
 const service = new PesaJs.LipaNaMpesa(require("../.test.local.json"));
 
+const cart = {
+    transaction: "54fgftgh",
+    ref: "ytrytfytf",
+    account: "254709998888",
+    amount: 8765,
+    callbackUrl: "http://awesome-store.co.ke/ipn",
+    details: "Additional transaction details if any"
+};
+
 describe("PaymentService", function () {
     
     
@@ -17,11 +26,24 @@ describe("PaymentService", function () {
         it("checks cart params", function (done) {
             service.requestPayment()
                 .then((data) => {
+                    data.should.not.exist();
+                    done();
+                })
+                .catch((err) => {
+                    expect(err).to.be.not.equal(null);
+                    done();
+                });
+        });
+
+        it("submits payment request", function (done) {
+            service.requestPayment(cart)
+                .then((data) => {
                     data.should.exist();
                     done();
                 })
                 .catch((err) => {
-                    expect(err).to.be.equal(null);
+                    expect(err).to.be.equal(null); // grr really?
+                    done();
                 });
         });
 
